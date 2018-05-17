3ebfebcda34a2cddf72c211d8834d36faf79a4bb
Sachin <sachi77n@gmail.com>
2018-05-17 20:40:43 +0530

    Fix #97
    Uncaught Error: Make sure that slide 0 has an image/video src

--- src/js/lightgallery.js
+++ src/js/lightgallery.js
@@ -430,10 +430,6 @@ Plugin.prototype.doCss = function() {
  */
 Plugin.prototype.isVideo = function(src, index) {
 
-    if(!src) {
-        throw new Error("Make sure that slide " + index + " has an image/video src");
-    }
-
     var html;
     if (this.s.dynamic) {
         html = this.s.dynamicEl[index].html;
