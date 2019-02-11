8d669b62e6a55960821ad5c2f5cdfed5689e996c
Sachin Neravath <sachi77n@gmail.com>
2019-02-11 10:22:42 +0530

    Update latest version

--- lightgallery/js/lightgallery.js
+++ lightgallery/js/lightgallery.js
@@ -1,4 +1,9 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Lightgallery = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/**!
+ * lightgallery.js | 1.1.3 | February 11th 2019
+ * http://sachinchoolur.github.io/lightgallery.js/
+ * Copyright (c) 2016 Sachin N; 
+ * @license GPLv3 
+ */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Lightgallery = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (global, factory) {
     if (typeof define === "function" && define.amd) {
         define(['exports'], factory);
@@ -18,26 +23,14 @@
         value: true
     });
 
-    /*
-     *@todo remove function from window and document. Update on and off functions
-     */
-    window.getAttribute = function (label) {
-        return window[label];
-    };
-
-    window.setAttribute = function (label, value) {
-        window[label] = value;
-    };
-
-    document.getAttribute = function (label) {
-        return document[label];
-    };
-
-    document.setAttribute = function (label, value) {
-        document[label] = value;
-    };
-
     var utils = {
+        getAttribute: function getAttribute(el, label) {
+            return el[label];
+        },
+
+        setAttribute: function setAttribute(el, label, value) {
+            el[label] = value;
+        },
         wrap: function wrap(el, className) {
             if (!el) {
                 return;
@@ -80,8 +73,6 @@
             } else {
                 return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
             }
-
-            return false;
         },
 
         // ex Transform
@@ -99,7 +90,7 @@
         },
 
         trigger: function trigger(el, event) {
-            var detail = arguments.length <= 2 || arguments[2] === undefined ? null : arguments[2];
+            var detail = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
 
             if (!el) {
                 return;
@@ -115,15 +106,17 @@
             uid: 0
         },
         on: function on(el, events, fn) {
+            var _this = this;
+
             if (!el) {
                 return;
             }
 
             events.split(' ').forEach(function (event) {
-                var _id = el.getAttribute('lg-event-uid') || '';
+                var _id = _this.getAttribute(el, 'lg-event-uid') || '';
                 utils.Listener.uid++;
                 _id += '&' + utils.Listener.uid;
-                el.setAttribute('lg-event-uid', _id);
+                _this.setAttribute(el, 'lg-event-uid', _id);
                 utils.Listener[event + utils.Listener.uid] = fn;
                 el.addEventListener(event.split('.')[0], fn, false);
             });
@@ -134,7 +127,7 @@
                 return;
             }
 
-            var _id = el.getAttribute('lg-event-uid');
+            var _id = this.getAttribute(el, 'lg-event-uid');
             if (_id) {
                 _id = _id.split('&');
                 for (var i = 0; i < _id.length; i++) {
@@ -145,14 +138,14 @@
                                 if (utils.Listener.hasOwnProperty(key)) {
                                     if (key.split('.').indexOf(_event.split('.')[1]) > -1) {
                                         el.removeEventListener(key.split('.')[0], utils.Listener[key]);
-                                        el.setAttribute('lg-event-uid', el.getAttribute('lg-event-uid').replace('&' + _id[i], ''));
+                                        this.setAttribute(el, 'lg-event-uid', this.getAttribute(el, 'lg-event-uid').replace('&' + _id[i], ''));
                                         delete utils.Listener[key];
                                     }
                                 }
                             }
                         } else {
                             el.removeEventListener(_event.split('.')[0], utils.Listener[_event]);
-                            el.setAttribute('lg-event-uid', el.getAttribute('lg-event-uid').replace('&' + _id[i], ''));
+                            this.setAttribute(el, 'lg-event-uid', this.getAttribute(el, 'lg-event-uid').replace('&' + _id[i], ''));
                             delete utils.Listener[_event];
                         }
                     }
@@ -560,7 +553,9 @@
             _lgUtils2.default.setVendor(inner, 'TransitionDuration', this.s.speed + 'ms');
         }
 
-        _lgUtils2.default.addClass(document.querySelector('.lg-backdrop'), 'in');
+        setTimeout(function () {
+            _lgUtils2.default.addClass(document.querySelector('.lg-backdrop'), 'in');
+        });
 
         setTimeout(function () {
             _lgUtils2.default.addClass(_this.outer, 'lg-visible');
@@ -630,7 +625,7 @@
             };
         }
 
-        var youtube = src.match(/\/\/(?:www\.)?youtu(?:\.be|be\.com)\/(?:watch\?v=|embed\/)?([a-z0-9\-\_\%]+)/i);
+        var youtube = src.match(/\/\/(?:www\.)?youtu(?:\.be|be\.com|be-nocookie\.com)\/(?:watch\?v=|embed\/)?([a-z0-9\-\_\%]+)/i);
         var vimeo = src.match(/\/\/(?:www\.)?vimeo.com\/([0-9a-z\-_]+)/i);
         var dailymotion = src.match(/\/\/(?:www\.)?dai.ly\/([0-9a-z\-_]+)/i);
         var vk = src.match(/\/\/(?:www\.)?(?:vk\.com|vkontakte\.ru)\/(?:video_ext\.php\?)(.*)/i);
@@ -1236,8 +1231,8 @@
                 prev.removeAttribute('disabled');
                 _lgUtils2.default.removeClass(prev, 'disabled');
             } else {
-                next.setAttribute('disabled', 'disabled');
-                _lgUtils2.default.addClass(next, 'disabled');
+                prev.setAttribute('disabled', 'disabled');
+                _lgUtils2.default.addClass(prev, 'disabled');
             }
         }
     };
@@ -1532,7 +1527,7 @@
         // Distroy all lightGallery modules
         for (var key in window.lgModules) {
             if (_this.modules[key]) {
-                _this.modules[key].destroy();
+                _this.modules[key].destroy(d);
             }
         }
 
